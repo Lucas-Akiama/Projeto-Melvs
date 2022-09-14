87453624b6d610ff7203ca528334edba8d33dd9b
gustavodesantanadev <gustavo.lima@sptech.school>
2022-09-13 21:50:54 -0300

    Banco de Dados

--- database/bdMelvs.sql
+++ database/bdMelvs.sql
@@ -1,37 +1,54 @@
-create database projeto_soja; 
-use projeto_soja; 
+CREATE DATABASE bdMelvs;
 
-create table cadastro( 
-id int primary key auto_increment, 
-Nome_responsável varchar(30) not null, 
-Nome_empresa varchar(30) not null, 
-CNPJ varchar (18) not null, 
-Email varchar (30), constraint chkemail check(Email like '%@%'),
-Senha varchar (30) not null,
-Estado_Cidade varchar (45) not null, 
-Endereço varchar(255) default 'Endereço' 
+USE bdMelvs;
+
+-- Criação das tabelas
+CREATE TABLE empresa(
+	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR (50) NOT NULL,
+    cnpj VARCHAR (18) NOT NULL, 
+	estado CHAR (2) NOT NULL,
+    cidade VARCHAR (45) NOT NULL, 
+	cep VARCHAR (14) NOT NULL,
+    complemento VARCHAR (15)
+);
+
+CREATE TABLE cadastro(
+	idCadastro INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(30) NOT NULL, 
+	email VARCHAR (30), CONSTRAINT chkemail CHECK (email LIKE '%@%'),
+	senha VARCHAR (30) NOT NULL,
+    cpf VARCHAR (15) NOT NULL,
+    idEmpresa INT,
+    FOREIGN KEY (idEmpresa) REFERENCES empresa(idEmpresa) 
+);
+    
+CREATE TABLE sensor( 
+	idSensor INT PRIMARY KEY AUTO_INCREMENT, 
+	dtSensor DATETIME DEFAULT CURRENT_TIMESTAMP, 
+	temperatura DOUBLE, 
+	umidade DOUBLE 
 ); 
-select * from cadastro; 
-insert into cadastro (nome_responsável, nome_empresa, CNPJ, Email, Senha, Estado_Cidade, Endereço) values 
-('Gilberto', 'Sojeando', '12.345.678/1234-12', 'gilberto.sojeando@uol.com', '#Gf123456', 'MG / Uberlândia', default),
-('Vivian', 'Vivando', '98.765.432/1342-13', 'vivian.vivando@gmail.com', '#Vi9876', 'GO / Goiânia', 'R. Marinho Gomes N° 123'),
-('Cleber', 'Soja Sempre', '56.677.834/4321-45', 'clebinho.souza@sojasempre.com', 'Cleber@2004', 'GO / Goiânia', default),
-('Ashley', 'Farm Soya', '52.555.968/9841-35', 'ashley.smith@farmsoya.com', 'Ash!19012016','MT / Sorriso', default);
 
- ----------------------------------------------------------------------------------------------------------------------------------------- 
+-- Exibição das Tabelas
+SELECT * FROM sensor;
 
-Create table sensor( 
-Id int primary key auto_increment, 
-Horário Datetime, 
-Temperatura double, 
-Umidade double 
-); 
+SELECT * FROM cadastro;
+    
+SELECT * FROM empresa;
 
-Insert into sensor (horário, temperatura, umidade)values 
-('2022-08-21 12:08:54', 27, 18),
-('2022-08-21 13:08:54', 25, 17),
-('2022-08-21 14:08:54', 26, 18),
-('2022-08-21 15:08:54', 24, 17),
-('2022-08-21 16:08:54', 22, 16);
-
-select * from sensor; 
+-- Inserção de dados
+INSERT INTO empresa(nome,cnpj,estado,cidade,cep) 
+	VALUES('SojasExpress','12.345.678/1234-12','SP','São Paulo','08151000'),
+			('SuperSoja','98.765.432/1342-13','MG','Boa Vista','50501000'),
+			('Farm Soya','52.555.968/9841-35','MT','Sorriso','12124000');
+    
+INSERT INTO cadastro (nome,email,senha,cpf,idEmpresa)
+	VALUES('Gustavo','gustavo@gmaisl.com','123456789','44785069813',1),
+			('Rafael','rafael@gmail.com','987654321','55066512341',2),
+			('Lucas','lucas@gmail.com','147852369','98855123614',3)	;
+    
+INSERT INTO sensor (temperatura,umidade)
+	VALUES (45,22),
+			(20,30),
+			(10,52);
